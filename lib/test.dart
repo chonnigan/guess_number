364c3471182262b1a7501ac5540e3a88c8251c24
chonnigan <khamwang_c@silpakorn.edu>
2021-12-13 16:03:32 +0700

    homework2

--- lib/test.dart
+++ lib/test.dart
@@ -2,17 +2,29 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
+  var i = 0;
   var r = Random();
-  var answer = r.nextInt(10);
+  var answer = r.nextInt(100);
+  int count = 0;
   while (true) {
     stdout.write("Please guess the number: ");
     var input = stdin.readLineSync();
     var guess = int.tryParse(input!);
     if (guess == answer) {
-      print ("CORRECT");
-      return;
+      print("CORRECT");
+      count++;
+      break;
     } else {
-      print("AGAIN");
+      if (guess! > answer) {
+        print('TRY AGAIN, $guess is TOO HIGH');
+        count++;
+      } else if (guess < answer) {
+        print('TRY AGAIN, $guess is TOO LOW');
+        count++;
+      }
     }
+  }
+  if (count >= 1) {
+    print('Total guesses: $count');
   }
 }
